8043986f3da1511e0992ff3437e131b672e0d943
Reefs1230 <chereifsmith@icloud.com>
2025-09-29 16:14:23 -0400

    Add files via upload

--- To-Do list/index.js
+++ To-Do list/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  // filtering state: 'all' | 'active' | 'complete'
+  var currentFilter = 'all';
+
   var getAndDisplayAllTasks = function () {
     $.ajax({
       type: 'GET',
@@ -6,8 +9,20 @@ $(document).ready(function(){
       dataType: 'json',
       success: function (response, textStatus) {
         $('#todo-list').empty();
-        response.tasks.forEach(function (task) {
-          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+  // optionally filter client-side to avoid extra API params
+  var tasksToShow = response.tasks.filter(function (task) {
+          if (currentFilter === 'all') return true;
+          if (currentFilter === 'active') return !task.completed;
+          if (currentFilter === 'complete') return task.completed;
+          return true;
+        });
+
+        tasksToShow.forEach(function (task) {
+          // add a class for completed tasks for styling
+          var completedClass = task.completed ? 'task-completed' : '';
+          var checkbox = '<input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>';
+          var deleteBtn = '<button class="delete" data-id="' + task.id + '">Delete</button>';
+          $('#todo-list').append('<div class="row todo-row"><p class="col-xs-8 ' + completedClass + '">' + escapeHtml(task.content) + '</p><div class="col-xs-4">' + deleteBtn + checkbox + '</div></div>');
         });
       },
       error: function (request, textStatus, errorMessage) {
@@ -94,6 +109,24 @@ $(document).ready(function(){
       markTaskActive($(this).data('id'));
     }
   });
+
+  // filter button click handler
+  $(document).on('click', '.filter-btn', function () {
+    $('.filter-btn').removeClass('active');
+    $(this).addClass('active');
+    currentFilter = $(this).data('filter');
+    getAndDisplayAllTasks();
+  });
+
+  // small helper to escape HTML in task content to avoid XSS if API returns raw HTML
+  function escapeHtml(text) {
+    return text
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
+  }
 
   getAndDisplayAllTasks();
 });
